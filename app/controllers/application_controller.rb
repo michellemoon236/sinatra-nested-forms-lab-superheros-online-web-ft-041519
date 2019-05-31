8e822909e8de21b3df855520349bf48c88bd9b63
Michelle Moon <michellemoon236@gmail.com>
2019-05-31 19:17:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,9 @@ class App < Sinatra::Base
   end
   
   post '/teams' do
-    @team = Team.new(params[team])
+    @team = Team.new(params[:team])
     
-    params[team][heroes].each do |hero_details|
+    params[:team][:heroes].each do |hero_details|
       Hero.new(hero_details)
     end
     
